6d5f2fed1e5f55e35b7fea533e71acc76aed1d58
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-02-08 20:28:59 -0700

    Util can compose asm cmds from ints

--- assembler/util.py
+++ assembler/util.py
@@ -5,3 +5,17 @@ def error(err, context):
 def nonASMError(err):
     print "- Comp16 Assembler - \nError:\n" + str(err)
     exit()
+
+def intsToCmd(vals, lens):
+    #vals is array of ints to make command, lens is length in bits of each
+    result = 0
+    lenI = 0
+    for i in vals:
+        cur = i & (2**(lens[lenI])-1)
+        result += cur
+        if lenI < len(lens)-1:
+            result <<= lens[lenI + 1]
+        lenI += 1
+    if result >= 65536:
+        error("Internal Error: to large values passed to intsToCmd in util.py", "None")
+    return chr(result >> 8) + chr(result & 255)
