d1bfec365025bf99836084639fb1059147ff578c
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-02-19 16:59:24 -0700

    assembler uses argparse

--- assembler/args.py
+++ assembler/args.py
@@ -2,50 +2,25 @@ import sys
 import argparse
 from util import *
 
-parser = argparse.ArgumentParser(prog='PROG', add_help=False)
+parser = argparse.ArgumentParser(usage="\n--------------------\n- Comp16 Assembler -\nUsage:\npython assemble.py [options] <asm file>\nOptions:\n-o output file  (default: asm.bin)\n-h, --help display   help\n--------------------")
+parser.add_argument("-o", nargs=1)
+parser.add_argument("asm_file")
 args = parser.parse_args()
 
-def getFlags():
-	index = 0
-	flags = []
-	for i in sys.argv:
-		if i[0] != "-":
-			index += 1
-			continue
-		flag = i[1:]
-		#Dont interpret asm file as flag arg
-		arg = False
-		if index + 1 < len(sys.argv)-1:
-			if sys.argv[index+1][0] != "-":
-				arg = sys.argv[index+1]
-		flags.append([flag, arg])
-		index += 1
-	return flags
-
 def print_usage():
-    print "- Comp16 Assembler -\nUsage:\npython assemble.py [options] <asm file>\nOptions:\n-o output file  (default: asm.bin)\n--------------------"
+    print "usage:\n--------------------\n- Comp16 Assembler -\nUsage:\npython assemble.py [options] <asm file>\nOptions:\n-o output file  (default: asm.bin)\n-h, --help   display help\n--------------------"
 
 def getFiles():
-    if len(sys.argv) < 2:
-        print_usage()
-        exit()
-
+    path = args.asm_file
+    outPath = 'asm.bin'
+    if args.o:
+        outPath = args.o
     try:
-        path = sys.argv[len(sys.argv)-1]
         asmFile = open(path)
     except IOError:
         print_usage()
         nonASMError("No such file: " + path)
     print "- Comp16 Assembler -"
-
-    flgs = getFlags()
-    outPath = 'asm.bin'
-    for i in flgs:
-    	if i[0] == 'o' and i[1]:
-    		outPath = i[1]
-    	if i[0] == 'o' and not i[1]:
-        	print_usage()
-         	nonASMError("-o flag requires and output file")
     outFile = open(outPath, 'w')
     print "Assembling " + path + " to " + outPath
     print "--------------------"
