aa12832678ddd7fa310f1aaf8b62fb66053a56dd
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-02-19 18:10:24 -0700

    Added makefile for install and uninstall

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-python assembler/c16asm.py
 chmod +x assembler/c16asm.py
 sudo ln -sf `pwd`/assembler/c16asm.py /usr/local/bin/c16asm
-python transmit/c16send.py
 chmod +x transmit/c16send.py
 sudo ln -sf `pwd`/transmit/c16send.py /usr/local/bin/c16send
