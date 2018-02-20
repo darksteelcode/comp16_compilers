8b14ad42dbd34f93a8be35b3712b7f8ba88e1626
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-02-19 17:44:53 -0700

    Converted transmiter to binary

--- transmit/transmit.py
+++ transmit/transmit.py
@@ -1,66 +1,74 @@
 import serial, sys, time, math
 import serial.tools.list_ports
+import argparse
 ser = serial.Serial()
 ser.baudrate = 115200
-
+ser.port = "FALSE"
 binFile = ""
 
-if len(sys.argv) < 2:
-	print "- Comp16 Prgm Send -\nUsage:\npython transmit.py [options] <bin file>\nOptions:\n-s serial_port  (default: searches for a port with an ftdi device on it)\n-b baudrate     (default: 115200)\n--------------------"
+parser = argparse.ArgumentParser(usage="\n--------------------\n- Comp16 Prgm Send -\nUsage:\npython transmit.py [options] <bin file>\nOptions:\n-s serial_port  (default: searches for a port with an uart device on it)\n-b baudrate     (default: 115200)\n--------------------")
+parser.add_argument("-s", nargs=1)
+parser.add_argument("-b", nargs=1)
+parser.add_argument("bin_file")
+args = parser.parse_args()
+print "- Comp16 Prgm Send -"
+binFile = args.bin_file
+if args.b:
+    ser.baudrate = int(args.b)
+if args.s:
+    ser.port = args.s[0]
+
+def print_usage():
+	print "\n--------------------\n- Comp16 Prgm Send -\nUsage:\npython transmit.py [options] <bin file>\nOptions:\n-s serial_port  (default: searches for a port with an uart device on it)\n-b baudrate     (default: 115200)\n--------------------"
 	exit()
 
 
 
 
-print "Hexedecimal Text File To Transmit: " + hexFile
-num_lines = sum(1 for line in open(hexFile, 'r'))
-f = open(hexFile, 'r')
+print "Transmiting " + binFile + " to " + ser.port
+num_lines = sum(1 for line in open(binFile, 'r'))
+f = open(binFile, 'r')
 
-print "No Port Specified, Looking for FTDI Port"
-foundOne = False
+if ser.port == "FALSE":
+	print "No Port Specified, Looking for Port with a UART on It"
 for p in serial.tools.list_ports.comports():
-    if "ft232r" in p[1].lower():
-        foundOne = True
+    if "uart" in p[1].lower() and ser.port == "FALSE":
         print "Found port " + str(p) + ".\nOpening to transmit"
         ser.port = p[0]
-        ser.open()
-        if ser.isOpen():
-            ser.read(ser.inWaiting())
-            print "Opened"
-        else:
-            print "Port Failed to open"
-            exit()
-if not foundOne:
-    print "No Port Specified, and no FTDI Port Found"
+
+if ser.port == "FALSE":
+    print "No Port Specified, and no UART Port Found. Terminating"
+    exit()
+ser.open()
+if ser.isOpen():
+    ser.read(ser.inWaiting())
+    print "Opened"
+else:
+    print "Port Failed to open"
     exit()
 
-def sendHex(hex):
-    confirm = ""
-    for d in range(len(hex)):
-        if d % 2 == 1:
-            data = bytearray.fromhex(hex[d-1:d+1])
-            ser.write(data)
-            confirm += hex[d-1:d+1]
+def sendBin(bin_data):
+    for d in bin_data:
+            ser.write(d)
             time.sleep(0.0001)
-    return confirm
+    return bin_data
 
-def confirmHex(hex_to_confirm):
+def confirmBin(bin_to_confirm):
     global errors
-    confirm_len = len(hex_to_confirm)/2
+    confirm_len = len(bin_to_confirm)
     while ser.inWaiting() < confirm_len:
         pass
-    data = ser.read(ser.inWaiting()).encode('hex')
-    good = data == hex_to_confirm
+    data = ser.read(ser.inWaiting())
+    good = data == bin_to_confirm
     if not good:
         errors += 1
         print "\n---------------------\nERROR\nBAD CONFIRMATION RECVIVED\n---------------------\n"
     return good
 
 start = time.time()
-hexData = f.read().replace("\n", "")
+binData = f.read().replace("\n", "")
 chunkLen = 128
-chunkLen = chunkLen * 2
-chunks = [hexData[i:i+chunkLen] for i in range(0, len(hexData), chunkLen)]
+chunks = [binData[i:i+chunkLen] for i in range(0, len(binData), chunkLen)]
 index = 0
 cur = 0
 num_chunks = len(chunks)
@@ -70,19 +78,20 @@ print "Transmitting"
 sys.stdout.write("|")
 for i in range(78):
     sys.stdout.write("-")
-sys.stdout.write("|")
+sys.stdout.write("|\n")
 sys.stdout.flush()
 
 for c in chunks:
-    conf = sendHex(c)
-    confirmHex(conf)
-    if int((float(index)/float(num_chunks))*80.0) >= cur:
+    conf = sendBin(c)
+    confirmBin(conf)
+    while int((float(index)/float(num_chunks))*80.0) >= cur:
         cur += 1
         sys.stdout.write("#")
         sys.stdout.flush()
     index+=1
 
-sendHex("ffff")
+sendBin("ffff")
+print
 ser.close()
 
 print "Program Sent"
