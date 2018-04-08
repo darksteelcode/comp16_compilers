994cdc4b06f0ccc5e0e6141721a4799881b97f4d
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-04-08 11:13:14 -0600

    MACROID's are replaced with uuid's in macro bodies

--- assembler/macro.py
+++ assembler/macro.py
@@ -1,5 +1,7 @@
 from util import *
 import vals
+#For macro ids
+import uuid
 '''
 Macro functions - this file contains the macro class, and tools to apply macro commands
 '''
@@ -32,6 +34,7 @@ class MacroVariation:
 
     #Apply args to body
     def applyArgs(self, args):
+        #put arg names and passed args in argNames and argVals
         argNames = []
         body = self.body
         for a in self.args:
@@ -40,14 +43,18 @@ class MacroVariation:
         for i in range(len(argVals)):
             if argVals[i][0] == '{' and argVals[i][-1] == '}':
                 argVals[i] = argVals[i][1:-1]
+        #Add MACROID's to argNames and argVals (16 MACROID's)
+        for n in range(16):
+            argNames.append("MACROID" + hex(n)[2:].upper())
+            argVals.append(uuid.uuid4().hex.upper())
+
         argIndex = 0
         for a in argNames:
             loc = body.find(a)
             while loc != -1:
                 endIndex = loc + len(a)
-                print body[loc:endIndex]
                 if (loc == 0 or body[loc-1] in vals.SEPARATORS) and (endIndex >= len(body) or body[endIndex] in vals.SEPARATORS):
                     body = body[:loc] + argVals[argIndex] + body[endIndex:]
-                loc = body.find(a)
+                loc = body.find(a, loc + 1)
             argIndex += 1
         return body
