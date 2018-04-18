80655c66926dbb968a306fa6ae2f2e96a36005b1
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-04-17 19:39:34 -0600

    Fixed makefile to install c16binToMif

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ chmod +x assembler/c16asm.py
 ln -sf `pwd`/assembler/c16asm.py /usr/local/bin/c16asm
 chmod +x transmit/c16send.py
 ln -sf `pwd`/transmit/c16send.py /usr/local/bin/c16send
-chmod +x binToMif/c16binToMif
+chmod +x binToMif/c16binToMif.py
 ln -sf `pwd`/binToMif/c16binToMif.py /usr/local/bin/c16binToMif
 mkdir -p /usr/c16-include
