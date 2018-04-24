ab1640170c25fa67f65004b0824cb570ffc76d4f
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-04-24 17:51:24 -0600

    stk emits the right opcode

--- assembler/cmds/stk.py
+++ assembler/cmds/stk.py
@@ -5,5 +5,5 @@ class stk(base.base):
     def initVars(self):
         self.types = [vals.TYPE_REG, vals.TYPE_VAL]
         self.lens = [4, 4, 8]
-        self.vals = [12]
+        self.vals = [13]
         self.LENGTH = 1
