f7d31f9088e741c25ba531d64a31e36030ff000e
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-04-24 17:49:41 -0600

    Added stk command

---
+++ assembler/cmds/stk.py
@@ -0,0 +1,9 @@
+import base
+import vals
+
+class stk(base.base):
+    def initVars(self):
+        self.types = [vals.TYPE_REG, vals.TYPE_VAL]
+        self.lens = [4, 4, 8]
+        self.vals = [12]
+        self.LENGTH = 1
